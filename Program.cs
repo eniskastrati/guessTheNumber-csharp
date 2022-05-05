39a2ed554344d12186aec29ae6c07bdff5ad6415
eniskastrati <eniskastrati18@gmail.com>
2022-05-05 23:26:55 +0200

    Asking the user to add then max and min number range

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace Game
         static void Main(string[] args)
         {
             //Game Title
-            Console.WriteLine("Github");
+            Console.WriteLine("********************");
+            Console.WriteLine("* Guess The Number *");
+            Console.WriteLine("********************\n");
+            
+            //Ask user for number range
+            Console.WriteLine("Please define the number range!");
+            Console.Write("Max Range-->");
+            int max_Range = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Min Range-->");
+            int min_Range = Convert.ToInt32(Console.ReadLine());
+            
+            //Functionality to generate a random Number
+            
             Console.ReadLine();
         }
     }
