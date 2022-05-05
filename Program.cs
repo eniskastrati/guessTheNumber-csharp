ea4b300686e40cb6a4f26158346638a9fcd4cdb5
eniskastrati <eniskastrati18@gmail.com>
2022-05-05 23:31:22 +0200

    Added functionality to generate the ranodm Number

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Game
     {
         static void Main(string[] args)
         {
+            Random randomNumber = new Random();
+
             //Game Title
             Console.WriteLine("********************");
             Console.WriteLine("* Guess The Number *");
@@ -18,8 +20,10 @@ namespace Game
             Console.Write("Min Range-->");
             int min_Range = Convert.ToInt32(Console.ReadLine());
             
-            //Functionality to generate a random Number
-            
+            //Functionality to generate a random Number between max and min Ranges
+            int magicNumber = randomNumber.Next(min_Range, max_Range);
+
+            Console.WriteLine(magicNumber);
             Console.ReadLine();
         }
     }
