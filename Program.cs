3df5c6b3124688113dbd0e4bcbecbf389d44bcf0
eniskastrati <eniskastrati18@gmail.com>
2022-05-05 23:11:42 +0200

    Creatin the Project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github");
+            Console.ReadLine();
+        }
+    }
+}
